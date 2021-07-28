0b9ef3ee556d51a46e5c1658d09bc88ce990b63c
Arun Singh <arunsingh28aug.as@gmail.com>
2021-07-28 17:25:08 +0530

    ReactNavigator setup

---
+++ src/Authentication/Onboarding.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+
+const ComponentName = () => {
+    return (
+        <View style={{ flex: 1, backgroundColor: 'cyan' }}>
+            <Text>Hello world</Text>
+        </View>
+    )
+}
+
+export default ComponentName
